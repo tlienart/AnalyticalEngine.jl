f71f82f7278773c85465bd399536bf7d053c5228
Thibaut Lienart <ltib@me.com>
2018-01-26 09:31:03 +0000

    first small flux model up

--- test/glr_fit.jl
+++ test/glr_fit.jl
@@ -1,7 +1,9 @@
 include("_using.jl")
 
+@testset "Fit GLR" begin
+
 # ---------------
-## OLS REGRESSION
+## OLS REGRESSION (analytical)
 # ---------------
 
 ols = LinearRegression()
@@ -12,7 +14,11 @@ srand(1234)
 n, p = 10, 3
 
 X = randn(n, p)
+X_ = hcat(ones(n, 1), X)
 y = randn(n)
+
+coefs = X_\y
+coefs_noi = X\y
 
 fit!(ols, X, y)
 fit!(ols_no_intercept, X, y)
@@ -20,8 +26,24 @@ fit!(ols_no_intercept, X, y)
 @test ols.n_features == p
 @test ols_no_intercept.n_features == p
 
-@test isapprox(ols.coefficients, hcat(ones(n, 1), X) \ y)
-@test isapprox(ols_no_intercept.coefficients, X \ y)
+@test isapprox(ols.coefficients, coefs)
+@test isapprox(ols_no_intercept.coefficients, coefs_noi)
+
+# ---------------
+## OLS REGRESSION (FLUX)
+# ---------------
+
+function update!(params) # basic gradient descent scheme
+    for param in params
+        param.data .-= 0.1param.grad
+        param.grad .= 0
+    end
+    params
+end
+
+fit!(ols, X, y, solver="Flux", update! = update!, nsteps=30)
+
+@test norm(ols.coefficients - coefs) <= 0.05
 
 # -----------------
 ## RIDGE REGRESSION
@@ -30,6 +52,10 @@ fit!(ols_no_intercept, X, y)
 λ = 2.0
 ridge = RidgeRegression(λ, fit_intercept=false)
 
+coefs_noi = (X' * X + λ * eye(p)) \ (X' * y)
+
 fit!(ridge, X, y)
 
-@test isapprox(ridge.coefficients, (X' * X + λ * eye(p)) \ (X' * y))
+@test isapprox(ridge.coefficients, coefs_noi)
+
+end # testset
--- test/glr_predict.jl
+++ test/glr_predict.jl
@@ -1,6 +1,9 @@
 include("_using.jl")
 
+@testset "Types GLR" begin
 
 lr = LinearRegression()
 
 lr.coefficients = [1., 2., 3.]
+
+end # testset
