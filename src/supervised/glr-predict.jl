b626f959852c41cda02bb2712d3201c89363f292
Thibaut Lienart <ltib@me.com>
2018-02-17 15:50:53 +0000

    get, set, copy

--- src/supervised/glr-predict.jl
+++ src/supervised/glr-predict.jl
@@ -1,12 +1,18 @@
 function predict(glr::GLR, X::AbstractArray{T}) where T<:Real
 
+    is_fitted = !any((glr.n_features, glr.intercept, glr.coefs) .== nothing)
+    @assert is_fitted "Model was not fitted"
+
     p = size(X, 2)
     @assert p == glr.n_features "Dimension mismatch"
 
-    if glr.fit_intercept
+    if get(glr.fit_intercept)
         X * glr.coefs .+ glr.intercept
     else
         X * glr.coefs
     end
 end
-(glr::GLR)(X) = predict(glr, X) 
+
+
+# convenience function to use a model directly as a function
+(glr::GLR)(X) = predict(glr, X)
--- src/supervised/glr-utils.jl
+++ src/supervised/glr-utils.jl
@@ -2,8 +2,13 @@
 function score(glr::GLR,
     X::AbstractArray{S}, y::AbstractVector{T}) where {S <: Real, T <: Real}
 
-    loss = glr.loss(predict(glr, X), y) / (glr.avgloss ? length(y) : 1)
-    penalty = glr.penalty(glr.coefs)
-    
+    # retrieve relevant hyperparameters
+    loss = get(glr.loss)
+    penalty = get(glr.penalty)
+    avg_loss = get(glr.avg_loss)
+
+    loss = loss(predict(glr, X), y) / (avg_loss ? length(y) : 1)
+    penalty = penalty(glr.coefs)
+
     return loss + penalty
 end
